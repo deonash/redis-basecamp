aaf5d288737e2f235d708dac12cb0b498f4c1c39
Avinash K P <avi.deonash@gmail.com>
2025-06-26 15:11:14 +0000

    springboot basic

---
+++ redis-springboot/src/main/java/com/techoral/redis/App.java
@@ -0,0 +1,14 @@
+package com.techoral.redis;
+
+/**
+ * www.techoral.com
+ *
+ */
+@SpringBootApplication
+public class RedisSpringbootApplication  
+{
+    public static void main( String[] args )
+    {
+         SpringApplication.run(RedisSpringbootApplication.class, args);
+    }
+}
