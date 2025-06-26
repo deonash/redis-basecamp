e647cce807027d7085abb9fa04ae5a30678f2790
Avinash K P <avi.deonash@gmail.com>
2025-06-26 15:32:33 +0000

    v01

--- redis-springboot/src/main/java/com/techoral/redis/App.java
+++ redis-springboot/src/main/java/com/techoral/redis/App.java
@@ -1,14 +1,23 @@
 package com.techoral.redis;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * www.techoral.com
  *
  */
 @SpringBootApplication
+@RestController
 public class RedisSpringbootApplication  
 {
     public static void main( String[] args )
     {
          SpringApplication.run(RedisSpringbootApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello from gipod springboot ...";
     }
 }
