bb1b39d7334c6c08c70770e973c968954e57ea24
Avinash K P <avi.deonash@gmail.com>
2025-06-28 13:26:20 +0000

    auth-service

---
+++ auth-service-rest/src/main/java/com/techoral/auth/api/AuthApiController.java
@@ -0,0 +1,47 @@
+package com.techoral.auth.api;
+
+import com.techoral.auth.model.LoginRequest;
+import com.techoral.auth.model.RegisterRequest;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import jakarta.validation.constraints.*;
+import jakarta.validation.Valid;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import jakarta.annotation.Generated;
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2025-06-28T13:22:44.475456814Z[Etc/UTC]", comments = "Generator version: 7.5.0")
+@Controller
+@RequestMapping("${openapi.auth.base-path:}")
+public class AuthApiController implements AuthApi {
+
+    private final NativeWebRequest request;
+
+    @Autowired
+    public AuthApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
---
+++ auth-service/src/main/java/com/techoral/auth/model/RegisterRequest.java
@@ -0,0 +1,144 @@
+package com.techoral.auth.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import jakarta.annotation.Generated;
+
+/**
+ * RegisterRequest
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2025-06-28T13:12:59.100362821Z[Etc/UTC]", comments = "Generator version: 7.5.0")
+public class RegisterRequest {
+
+  private String email;
+
+  private String password;
+
+  private String fullName;
+
+  public RegisterRequest() {
+    super();
+  }
+
+  /**
+   * Constructor with only required parameters
+   */
+  public RegisterRequest(String email, String password, String fullName) {
+    this.email = email;
+    this.password = password;
+    this.fullName = fullName;
+  }
+
+  public RegisterRequest email(String email) {
+    this.email = email;
+    return this;
+  }
+
+  /**
+   * Get email
+   * @return email
+  */
+  @NotNull @jakarta.validation.constraints.Email 
+  @Schema(name = "email", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("email")
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public RegisterRequest password(String password) {
+    this.password = password;
+    return this;
+  }
+
+  /**
+   * Get password
+   * @return password
+  */
+  @NotNull 
+  @Schema(name = "password", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("password")
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public RegisterRequest fullName(String fullName) {
+    this.fullName = fullName;
+    return this;
+  }
+
+  /**
+   * Get fullName
+   * @return fullName
+  */
+  @NotNull 
+  @Schema(name = "fullName", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("fullName")
+  public String getFullName() {
+    return fullName;
+  }
+
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    RegisterRequest registerRequest = (RegisterRequest) o;
+    return Objects.equals(this.email, registerRequest.email) &&
+        Objects.equals(this.password, registerRequest.password) &&
+        Objects.equals(this.fullName, registerRequest.fullName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(email, password, fullName);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class RegisterRequest {\n");
+    sb.append("    email: ").append(toIndentedString(email)).append("\n");
+    sb.append("    password: ").append("*").append("\n");
+    sb.append("    fullName: ").append(toIndentedString(fullName)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
