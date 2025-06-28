ab9bf88dfc3238d76aa8a76f3c37a9fb10b17927
Avinash K P <avi.deonash@gmail.com>
2025-06-28 10:17:34 +0000

    logs updated

--- redis-springboot/src/main/java/com/techoral/redis/config/RedisConfig.java
+++ redis-springboot/src/main/java/com/techoral/redis/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.techoral.redis.config;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -10,9 +11,15 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 @Configuration
 public class RedisConfig{
 
+        @Value("${spring.redis.host}")
+        private String redisHost;
+
+        @Value("${spring.redis.port}")
+        private int redisPort;
+
      @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
         return new LettuceConnectionFactory(config);
     }
 
